3e6afb47a2760270d82199d3867d2aea67561454
anisha-shamsudeen <anisha.shamsudeen@edu.esiee.fr>
2025-11-06 10:49:32 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+""" 
+Module contenant une fonction qui teste si le nombre passé en paramètre est premier.
+"""
+
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
+    """
+    Retourne la vérité de la proposition : "p est premier".
 
-    # votre code ici
-
-    pass
+    Args:
+        p: le nombre à tester pour savoir s'il est premier.
+    
+    Returns:
+        True: si p est premier.
+        False: si p n'est pas premier.
+    """
+    premier = True
+    if p in (0,1):
+        premier = False
+        return premier
+    racine = int(sqrt(p))
+    for d in range(2,racine + 1):
+        if p % d == 0:
+            premier = False
+    return premier
 
 #### Fonction principale
 
-
 def main():
-
+    """ appel de la fonction isprime(p) sur l'intervalle [0,99] """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
